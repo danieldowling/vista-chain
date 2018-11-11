f282f6a41b1c1f066adf5f2a272075066c37e0a1
danieldowling <ddowling30@gmail.com>
2018-11-10 17:35:00 -0800

    added endpoints for creating transactions

--- server/index.js
+++ server/index.js
@@ -3,14 +3,17 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 const path = require('path');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
-const wallet = new wallet();
 
 app.use(cors(), bodyParser.json());
 
@@ -25,6 +28,16 @@ app.post('/mine', (req, res) => {
     p2pServer.syncChains();
 
     res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    res.redirect('/transactions');
 });
 
 app.listen(HTTP_PORT, () => {
